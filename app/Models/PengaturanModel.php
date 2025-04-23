443f5fc06cbb59e3cf2b4eb1634de03da725b8ee
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-04-23 08:56:50 +0700

    Reconfigure Pengaturan

--- app/Models/PengaturanModel.php
+++ app/Models/PengaturanModel.php
@@ -12,15 +12,9 @@ class PengaturanModel extends Model
     protected $useSoftDeletes = false;
     protected $protectFields = true;
     protected $allowedFields = [
-        'id_pengaturan', 'id_app', 'website', 'judul', 'judul_app', 'url_app', 'logo', 
-        'logo_header', 'logo_header_kop', 'deskripsi', 'deskripsi_pendek', 'notifikasi', 
-        'alamat', 'kota', 'email', 'pesan', 'tlp', 'fax', 'url_antrian', 'ss_org_id', 
-        'ss_client_id', 'ss_client_secret', 'kode_surat_sht', 'kode_surat_skt', 
-        'kode_surat_rnp', 'kode_surat_kntrl', 'kode_surat_lahir', 'kode_surat_mati', 
-        'kode_surat_covid', 'kode_surat_rujukan', 'kode_surat_tht', 'kode_surat', 
-        'kode_resep', 'kode_rad', 'kode_periksa', 'kode_pasien', 'ppn', 'ppn_tot', 
-        'jml_ppn', 'jml_item', 'jml_limit_stok', 'jml_limit_tempo', 'jml_poin', 
-        'jml_poin_nom', 'tahun_poin', 'status_app'
+        'judul', 'judul_app', 'alamat', 'deskripsi', 'kota', 
+        'url', 'theme', 'pagination_limit', 'favicon', 'logo', 
+        'logo_header', 'apt_apa', 'apt_sipa', 'ppn'
     ];
 
     // Dates
