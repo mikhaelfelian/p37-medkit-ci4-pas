d4ad3598ee74acb6ec6c5b408f45d7b4c5a0ba91
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-04-23 08:59:01 +0700

    Reconfigure App Setting

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/p37-medkit-ci4-pas/';
+    public string $baseURL = '';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
