4ed90583b2e6288e58d896802b661ae7e61b743d
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-04-24 21:06:12 +0700

    Configure ENV on view

--- app/Views/admin-lte-2/layout/footer.php
+++ app/Views/admin-lte-2/layout/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
   <div class="pull-right hidden-xs">
-    <b>Version</b> 2.4.18
+    <b>Version</b> <?= env('app.version', '3.0.2') ?>
   </div>
   <strong>Copyright &copy; <?= date('Y') ?> <a href="<?= $Pengaturan->website ?>"><?= $Pengaturan->judul ?></a>.</strong> All rights
   reserved.
